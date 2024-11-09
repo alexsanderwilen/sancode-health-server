05a1282d5e062e82180a1319c0298a322048e72a
Alexsander Wilen <alexsanderwilen@sancode.com.br>
2024-11-09 12:24:10 -0300

    Ajustado a montagem dos filtros, do where para o prisma

--- sancode-health-server/src/utils/filter.utils.ts
+++ sancode-health-server/src/utils/filter.utils.ts
@@ -30,31 +30,70 @@ export function buildWhereCondition(filters: Filter[], operador: 'AND' | 'OR' = 
         }
 
         switch (condicao) {
-            case 'começa com':
-                condition = { [campo]: { startsWith: valor, mode: 'insensitive' } };
-                break;
-                case 'termina com':
-                    condition = { [campo]: { endWith: valor, mode: 'insensitive' } };
-                    break;
             case 'igual':
-                // Caso seja igual, deve comparar o tipo do campo
-                if (tipo === 'number') {
-                    condition = { [campo]: { equals: parseFloat(valor) } };
-                } else {
-                    condition = { [campo]: { equals: valor, mode: 'insensitive'} };
-                }
+              // Caso seja igual, deve comparar o tipo do campo
+              if (tipo === 'number') {
+                  condition = { [campo]: { equals: parseFloat(valor) } };
+              } else {
+                  condition = { [campo]: { equals: valor, mode: 'insensitive'} };
+              }
+              break;
+            case 'diferente':
+                condition = { [campo]: { not: { equals: valor } } };
                 break;
-            case 'contém':  // Adicionando a condição "contém"
+                case 'contém':  // Adicionando a condição "contém"
                 condition = { [campo]: { contains: valor, mode: 'insensitive' } };
+                break;  
+            case 'começa com':
+                condition = { [campo]: { startsWith: valor, mode: 'insensitive' } };                     
+                break;
+            case 'termina com':
+                condition = { [campo]: { endsWith: valor, mode: 'insensitive' } };                            
                 break;
             case 'maior':
-                condition = { [campo]: { gt: parseFloat(valor) } };
+                if (tipo === 'number') {
+                    condition = { [campo]: { gt: parseFloat(valor) } };                     
+                } else if (tipo === 'date') {
+                    condition = { [campo]: { gt: new Date(valor) } };
+                } else {  
+                    condition = { [campo]: { gt: valor } };
+                }    
                 break;
             case 'menor':
-                condition = { [campo]: { lt: parseFloat(valor) } };
+                if (tipo === 'number') {
+                    condition = { [campo]: { lt: parseFloat(valor) } };                     
+                } else if (tipo === 'date') {
+                    condition = { [campo]: { lt: new Date(valor) } };
+                } else {  
+                    condition = { [campo]: { lt: valor } };
+                }    
+                break;
+            case 'igual ou maior':
+              if (tipo === 'number') {
+                condition = { [campo]: { gte: parseFloat(valor) } };
+              } else if (tipo === 'date') {
+                condition = { [campo]: { gte: new Date(valor) } };
+              } else {
+                condition = { [campo]: { gte: valor } };
+              }
+                break;
+            case 'igual ou menor':
+              if (tipo === 'number') {
+                condition = { [campo]: { lte: parseFloat(valor) } };
+              } else if (tipo === 'date') {
+                condition = { [campo]: { lte: new Date(valor) } };
+              } else {
+                condition = { [campo]: { lte: valor } };
+              }
                 break;
             case 'entre':
-                condition = { [campo]: { gte: parseFloat(valorInicial), lte: parseFloat(valorFinal) } };
+                if (tipo === 'number') {
+                    condition = { [campo]: { gte: parseFloat(valorInicial), lte: parseFloat(valorFinal) } };
+                } else if (tipo === 'date') {
+                    condition = { [campo]: { gte: new Date(valorInicial), lte: new Date(valorFinal) } };
+                } else {
+                    condition = { [campo]: { gte: valorInicial, lte: valorFinal } };
+                }
                 break;
             default:
                 throw new Error(`Condição '${condicao}' não suportada.`);
