f418f7081b9b7d930fc938d208ef2df8e8a5bc07
Alexsander Wilen <alexsanderwilen@sancode.com.br>
2024-11-16 22:43:04 -0300

    tentando configurar o cors para acesso da aplicação cliente

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://sancode-health-cadastro.vercel.app',
+    methods: 'GET, POST, PUT, DELETE, PATCH', 
+    allowedHeaders: 'Content-Type, Accept, Authorization', 
+  });
   await app.listen(3000);
 }
 bootstrap();
