4a00a4a3a20b1ffad230beb762a24b9017044d29
Alexsander Wilen <alexsanderwilen@sancode.com.br>
2024-11-20 15:04:31 -0300

    adicionado a importaçãodos modulos Estado, Municipio, Bairros e CEPs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PaisesModule } from './paises/paises.module';
 import { RegioesModule } from './regioes/regioes.module';
+import { EstadosModule } from './estados/estados.module';
+import { MunicipiosModule } from './municipios/municipios.module';
+import { BairrosModule } from './bairros/bairros.module';
+import { CepsModule } from './ceps/ceps.module';
 
 @Module({
-  imports: [AuthModule, PaisesModule, RegioesModule],
+  imports: [AuthModule, PaisesModule, RegioesModule, EstadosModule, MunicipiosModule, BairrosModule, CepsModule],
   controllers: [AppController],
   providers: [AppService],
 })
