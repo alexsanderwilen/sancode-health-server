12f8d9e252a7a7489c91df06035f125dbf8d3d66
Alexsander Wilen <alexsanderwilen@sancode.com.br>
2024-11-24 21:26:00 -0300

    refatorado para trazer a regição e o pais também no create

--- src/estados/estados.service.ts
+++ src/estados/estados.service.ts
@@ -79,8 +79,7 @@ export class EstadosService {
         if (!estado) {
             throw new Error(`Estado com UF ${uf} não encontrado.`);
         }
-    
-        // Retorna com aliases
+            
         return {
             id: estado.id,
             codigoUf: estado.codigo_uf,
@@ -109,9 +108,25 @@ export class EstadosService {
         }
     
         // Cria o estado
-        return this.prisma.tb_estado.create({
+        const estado = await this.prisma.tb_estado.create({
             data,
+            include: {
+                tb_regiao: { select: { regiao: true } },
+                tb_pais: { select: { pais: true } },
+            },
         });
+                       
+        return {
+            id: estado.id,
+            codigoUf: estado.codigo_uf,
+            estado: estado.estado,
+            estadoOficial: estado.estado_oficial,
+            uf: estado.uf,
+            regiaoId: estado.regiao_id,
+            regiao: estado.tb_regiao?.regiao,
+            paisId: estado.pais_id,
+            pais: estado.tb_pais?.pais,
+        };
     }
     
 
