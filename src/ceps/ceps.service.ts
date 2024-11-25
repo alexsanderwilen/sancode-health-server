f715968e8b2a2ce8b04f6f1bd9dfd129db4f7d7d
Alexsander Wilen <alexsanderwilen@sancode.com.br>
2024-11-24 21:26:58 -0300

    Refatorado para organizar os nomes dos fields e trazer as infomações do municipio e estado

--- src/ceps/ceps.service.ts
+++ src/ceps/ceps.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { buildWhereCondition, Filter } from '../utils/filter.utils';
+import { BairrosController } from 'src/bairros/bairros.controller';
 
 @Injectable()
 export class CepsService {
@@ -9,29 +10,95 @@ export class CepsService {
     async findAll(filtroExtra: Filter[]) {
         try {
             const where = buildWhereCondition(filtroExtra);
-            const [data] = await Promise.all([
-                this.prisma.tb_cep.findMany({
-                    orderBy: { cep: 'asc' },
-                    where
-                })
-            ]);
-
-            return {
-                data,
-            };
+    
+            const ceps = await this.prisma.tb_cep.findMany({
+                orderBy: { logradouro: 'asc' },
+                where,
+                include: {
+                    tb_municipio: { select: { cidade: true } },
+                    tb_estado: { select: { estado: true, uf: true } },
+                },
+            });
+                
+            const data = ceps.map((cep) => ({
+                id: cep.id,
+                cep: cep.cep,
+                logradouro: cep.logradouro,
+                faixaCep: cep.faixa_cep,
+                bairro: cep.bairro,
+                cidadeId: cep.cidade_id,
+                cidade: cep.tb_municipio?.cidade,
+                estadoId: cep.estado_id,
+                estado: cep.tb_estado?.estado,
+                uf: cep.tb_estado?.uf,
+            }));
+    
+            return { data };
         } catch (error) {
-            console.error('Erro ao buscar ceps:', error);
+            console.error('Erro ao buscar os CEPs:', error);
             throw error;
         }
     }
 
     async FindOne(id: number) {
-        return this.prisma.tb_cep.findUnique({ where: { id } });
+        const cep = await this.prisma.tb_cep.findUnique({
+            where: { id },
+            include: {
+                tb_municipio: { select: { cidade: true } },
+                tb_estado: { select: { estado: true, uf: true } },             
+            },
+        });
+    
+        if (!cep) {
+            throw new Error(`CEP com ID ${id} não encontrado.`);
+        }
+    
+        return {
+            id: cep.id,
+            cep: cep.cep,
+            logradouro: cep.logradouro,
+            faixaCep: cep.faixa_cep,
+            bairro: cep.bairro,
+            cidadeId: cep.cidade_id,
+            cidade: cep.tb_municipio?.cidade,
+            estadoId: cep.estado_id,
+            estado: cep.tb_estado?.estado,
+            uf: cep.tb_estado?.uf,
+        };
     }
+    
 
     async findByCep(cep: string) {
-        return this.prisma.tb_cep.findMany({ where: { cep } });
+        try {    
+            const ceps = await this.prisma.tb_cep.findMany({
+                orderBy: { logradouro: 'asc' },
+                where: { cep },
+                include: {
+                    tb_municipio: { select: { cidade: true } },
+                    tb_estado: { select: { estado: true, uf: true } },
+                },
+            });
+                
+            const data = ceps.map((cep) => ({
+                id: cep.id,
+                cep: cep.cep,
+                logradouro: cep.logradouro,
+                faixaCep: cep.faixa_cep,
+                bairro: cep.bairro,
+                cidadeId: cep.cidade_id,
+                cidade: cep.tb_municipio?.cidade,
+                estadoId: cep.estado_id,
+                estado: cep.tb_estado?.estado,
+                uf: cep.tb_estado?.uf,
+            }));
+    
+            return { data };
+        } catch (error) {
+            console.error('Erro ao buscar os CEPs:', error);
+            throw error;
+        }
     }
+
 
     async create(data: { cep: string; logradouro: string; faixa_cep: string; bairro: string; cidade_id: number; estado_id: number }) {
         return this.prisma.tb_cep.create({ data });
